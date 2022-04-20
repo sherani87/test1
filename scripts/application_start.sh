0248665718dfe482fcd0c9ac35306d852fdee2fa
sherani87 <95246669+sherani87@users.noreply.github.com>
2022-04-20 13:19:43 -0400

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@
 
 sudo systemctl start apache2
 sudo systemctl enable apache2
-sudo echo "This is saeed html">/var/www/html/index.html
+sudo echo "This is saeed html second time">/var/www/html/index.html
