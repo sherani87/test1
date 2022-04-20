466e617cddb415bdb8241e21e6b10d79e7815746
sherani87 <95246669+sherani87@users.noreply.github.com>
2022-04-20 12:46:36 -0400

    Update application_start.sh
    need to change the name of apache server

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,6 +3,6 @@
 #Starting the httpd server
 
 sudo su
-systemctl start httpd
-systemctl enable httpd
+systemctl start apache2 -y
+systemctl enable apache2
 echo "This is saeed html">/var/ww/html/index.html
